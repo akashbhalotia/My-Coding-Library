24fcda9fdd2698568b9c0b70673be223905a3635
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-03-04 21:09:17 +0530

    Create Z-Function.java

---
+++ Strings/Z-Function.java
@@ -0,0 +1,21 @@
+/*
+  O(N) to create the Z-function array.
+  z[i] = longest common prefix between the main string and the suffix starting at i.
+*/
+
+private static void computeZArr(char str[], int z[])
+{
+    int l=0,r=0,N=str.length;
+    for(int i=1;i<N;i++)
+    {
+        if(i<=r)
+        z[i]=Math.min(r-i+1,z[i-l]);
+        while (i+z[i]<N&&str[i]==str[i+z[i]])
+            ++z[i];
+        if(i+z[i]-1>r)
+        {
+            l=i;
+            r=i+z[i]-1;
+        }
+    }
+}
