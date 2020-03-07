7f093f832d198800bc8af27530a4383838bf7bfd
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-03-07 16:53:55 +0530

    Create KMP.java

---
+++ Strings/KMP.java
@@ -0,0 +1,15 @@
+/*
+	O(N) to create the pi array.
+	pi[i]=lenegth of the longest proper suffix ending at i, which is also a proper prefix.
+*/
+
+private static void calcPi(char str[], int pi[])
+{
+	int N=str.length,j=0;
+    for(int i=1;i<N;i++)
+	{
+        while (j>0&&str[i]!=str[j])j=pi[j];
+		if(str[i]==str[j])j++;
+        pi[i]=j;
+    }
+}
