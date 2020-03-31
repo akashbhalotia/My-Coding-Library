0c3d920ff6c3349c0f4832021a1f0e0c9c0a202e
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-03-31 17:49:30 +0530

    Update KMP.java

--- Strings/KMP.java
+++ Strings/KMP.java
@@ -14,3 +14,26 @@ private static void calcPi(char str[], int pi[], int N)
         pi[i] = j;
     }
 }
+
+/*
+    To perform pattern matching on a text, one way is to make a new string: pattern+'$'+text.
+    Then compute pi for this string, and print the positions which have a pi value of pattern.length
+*/
+
+public static int countMatches1(char text[], char pat[])
+{
+    String s=new String(pat)+"$"+new String(text);
+    int M=text.length, N=pat.length, count=0;
+    int[] pi=new int[M+N+1];
+    calcPi(s.toCharArray(),pi,M+N+1);
+
+    for(int i=N+1;i<M+N+1;i++)
+    {
+        if(pi[i]==N)
+        {
+            System.out.println("Found at index "+(i-2*N)); //0-indexed
+            count++;
+        }
+    }
+    return count;
+}
