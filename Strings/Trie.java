f136723199de48064c6848035e8bfc3046bb04a2
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-04-02 15:21:08 +0530

    Update Trie.java

--- Strings/Trie.java
+++ Strings/Trie.java
@@ -2,13 +2,14 @@
   Basic Trie. Insert strings and check if they are present in the trie.
   
   To add a string: insert(char str[]). 
-  To check if a string is present: exists(char str[]).
+  To check if a string is present: contains(char str[]).
+  To clear the trie: clear().
   
   Inserting takes O(ALPHA*N) total, where ALPHA is the size of the alphabet, and N 
   is the total length of all the input strings. To make it O(N), we can use a Hashmap, 
   but it may have high overheads. 
   
-  Checking if a string exists just takes O(M), where M is the size of the string to be searched.
+  Checking if a string exists in the trie just takes O(M), where M is the size of the string to be searched.
 */
 
 private static final int ALPHA=26;
@@ -47,7 +48,7 @@ static class Trie
         }
         trie.get(v).isLeaf=true;
     }
-    boolean exists(char str[])
+    boolean contains(char str[])
     {
         int v=root;
         for(char ch:str)
@@ -58,4 +59,5 @@ static class Trie
         }
         return trie.get(v).isLeaf;
     }
+    void clear(){trie.clear(); trie.add(new Node());}
 }
