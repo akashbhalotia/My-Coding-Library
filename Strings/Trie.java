2aa78f832ba9363a68893886275ee9a9891b4033
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-04-02 14:42:25 +0530

    Create Trie.java

---
+++ Strings/Trie.java
@@ -0,0 +1,61 @@
+/*
+  Basic Trie. Insert strings and check if they are present in the trie.
+  
+  To add a string: insert(char str[]). 
+  To check if a string is present: exists(char str[]).
+  
+  Inserting takes O(ALPHA*N) total, where ALPHA is the size of the alphabet, and N 
+  is the total length of all the input strings. To make it O(N), we can use a Hashmap, 
+  but it may have high overheads.
+  
+  Checking if a string exists just takes O(M), where M is the size of the string to be searched.
+*/
+
+private static final int ALPHA=26;
+static class Trie
+{
+    int root=0;
+    private ArrayList<Node> trie;
+    private class Node
+    {
+        int next[];
+        boolean isLeaf;
+
+        Node()
+        {
+            next=new int[ALPHA];Arrays.fill(next,-1);
+            isLeaf=false;
+        }
+    }
+    Trie()
+    {
+        trie =new ArrayList<>();
+        trie.add(new Node());
+    }
+    void insert(char str[])
+    {
+        int v=root;
+        for(char ch:str)
+        {
+            int c=ch-'a';
+            if(trie.get(v).next[c]==-1)
+            {
+                trie.get(v).next[c]= trie.size();
+                trie.add(new Node());
+            }
+            v=trie.get(v).next[c];
+        }
+        trie.get(v).isLeaf=true;
+    }
+    boolean exists(char str[])
+    {
+        int v=root;
+        for(char ch:str)
+        {
+            int c=ch-'a';
+            if(trie.get(v).next[c]==-1) return false;
+            v=trie.get(v).next[c];
+        }
+        return trie.get(v).isLeaf;
+    }
+}
