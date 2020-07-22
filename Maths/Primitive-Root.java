212711661286bb5ba657a6302408a6ce0146a21f
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-07-22 13:43:58 +0530

    Update Primitive-Root.java

--- Maths/Primitive-Root.java
+++ Maths/Primitive-Root.java
@@ -3,7 +3,7 @@
   
   Primitive root, g, of a number N is a number that generates all numbers coprime to N, mod N. 
   In other words, g is a primitive root of N, if for all a such that gcd(a,N)=1, there exists an integer k,
-  such that g^k=a mod n.
+  such that g^k=a mod N.
   
   Primitive roots exist iff N=1, 4, Prime, (odd prime)^k, 2*(odd prime)^k.
   
@@ -11,8 +11,8 @@
   _factorization-> gives phi(N) if mode is false, 
                    else gives a list of all prime factors of N, in O(sqrt(N)).
   
-  root is O(log^6 n).
-  Run Time of algorithm is O(root * log phi(n) * log n), which is approximately O(log^8 n).
+  root is O(log^6 N).
+  Run Time of algorithm is O(root * log phi(N) * log N), which is approximately O(log^8 N).
   
   https://cp-algorithms.com/algebra/primitive-root.html
 */
