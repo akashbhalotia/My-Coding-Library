07ad07687939aa090eb4c18fef05f1120a01d84f
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-10-15 23:58:29 +0530

    Update LongestIncreasingSubsequence.java

--- Maths/LongestIncreasingSubsequence.java
+++ Maths/LongestIncreasingSubsequence.java
@@ -1,7 +1,8 @@
 /*
   TODO.
   
-  For now, refer to these: 
+  For now, refer to these (these are longest non-decreasing, so modify if needed): 
+  
   1) https://codeforces.com/contest/582/submission/95606602
   2) Using max-BIT (Only works if the numbers fit or can be compressed): https://codeforces.com/contest/582/submission/95612769
   
