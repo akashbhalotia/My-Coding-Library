8bdec50e03c7f5aef8ba0795c10c715a8e3ae2c2
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-10-29 01:39:49 +0530

    Update LongestIncreasingSubsequence.java

--- Maths/LongestIncreasingSubsequence.java
+++ Maths/LongestIncreasingSubsequence.java
@@ -3,8 +3,9 @@
   
   For now, refer to these (these are longest non-decreasing, so modify if needed): 
   
-  1) https://codeforces.com/contest/582/submission/95606602 or https://codeforces.com/contest/1437/submission/97034944
-  2) Using max-BIT (Only works if the numbers fit or can be compressed): https://codeforces.com/contest/582/submission/95612769
+  1) https://codeforces.com/contest/1437/submission/97034944
+  2) https://codeforces.com/contest/582/submission/95606602
+  3) Using max-BIT (Only works if the numbers fit or can be compressed): https://codeforces.com/contest/582/submission/95612769
   
   Will add code later, if needed.
 */
