d9181c7592796aa70c190437245117ac425237bb
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-11-08 01:20:56 +0530

    Create LCS.java

---
+++ DP/LCS.java
@@ -0,0 +1,10 @@
+/*
+  Find the Longest common subsequence between two arrays in O(NlogN)
+  Solution: https://codeforces.com/blog/entry/13631?#comment-186010
+  
+  Only works if the elements of at least one sequence are distinct.
+  
+  
+  Find the longest common subsequence between K arrays: https://codeforces.com/problemset/problem/463/D
+  Only works when the elements of each array are distinct.
+*/
