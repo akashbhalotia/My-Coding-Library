88c481a05ef46d66b8e9d6bc5fe548bd619e4c7e
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-11-10 23:39:09 +0530

    Update Z-Function.java

--- Strings/Z-Function.java
+++ Strings/Z-Function.java
@@ -5,17 +5,17 @@
 
 private static void computeZArr(char str[], int z[])
 {
-    int l=0,r=0,N=str.length;
-    for(int i=1;i<N;i++)
+    int l = 0, r = 0, N = str.length;
+    for (int i = 1; i < N; i++)
     {
-        if(i<=r)
-          z[i]=Math.min(r-i+1,z[i-l]);
-        while (i+z[i]<N&&str[z[i]]==str[i+z[i]])
-           ++z[i];
-        if(i+z[i]-1>r)
+        if (i <= r)
+            z[i] = Math.min(r - i + 1, z[i - l]);
+        while (i + z[i] < N && str[z[i]] == str[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
         {
-            l=i;
-            r=i+z[i]-1;
+            l = i;
+            r = i + z[i] - 1;
         }
     }
 }
