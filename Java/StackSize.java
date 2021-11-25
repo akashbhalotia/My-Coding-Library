7d50e782f3bd41346a2387bca6bb39a5c66d1653
Akash Bhalotia <akashbhalotia10@gmail.com>
2021-11-25 21:44:57 +0530

    Update StackSize.java

--- Java/StackSize.java
+++ Java/StackSize.java
@@ -24,6 +24,6 @@ public class A implements Runnable
     public void run(){try{general();} catch (Exception e){}}
     public static void main(String[] args) throws Exception
     {
-        new Thread(null, new A(), "A", 1<<26);
+        new Thread(null, new A(), "A", 1<<26).start();
     }
 }
